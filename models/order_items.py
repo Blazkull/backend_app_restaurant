f37f3616f781e20325fc416e26129acf40d79c77
JHOANN ENIRQUE ACOSTA AMARANTO <159073532+Blazkull@users.noreply.github.com>
2025-10-21 19:16:04 -0500

    Update order_items.py
    remote column id_kitchen

--- models/order_items.py
+++ models/order_items.py
@@ -12,7 +12,7 @@ class OrderItems(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     id_order: int = Field(foreign_key="orders.id")
     id_menu_item: int = Field(foreign_key="menu_items.id")
-    id_kitchen_ticket: Optional[int] = Field(default=None, foreign_key="kitchen_tickets.id")
+    #id_kitchen_ticket: Optional[int] = Field(default=None, foreign_key="kitchen_tickets.id")
 
     quantity: int = Field(gt=0, description="Cantidad del ítem en el pedido")
     note: Optional[str] = Field(default=None, max_length=100, description="Nota especial para el ítem")
